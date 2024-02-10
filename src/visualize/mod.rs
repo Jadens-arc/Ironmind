19ad4eac3d8637eccb77c8e1e1ae90944bc4134c
Jaden Arceneaux <arceneauxjaden@gmail.com>
2024-02-10 13:11:54 -0800

    moved into step function

--- src/visualize/mod.rs
+++ src/visualize/mod.rs
@@ -3,6 +3,35 @@ use cursive::{Cursive, CursiveExt};
 use cursive::view::{Nameable, Resizable};
 use cursive::views::{Button, Dialog, LinearLayout, ScrollView, TextView};
 use crate::parser::Parser;
+
+fn step(cursive: &mut Cursive) {
+    if let Some(data) = cursive.user_data::<Parser>() {
+        if data.instruction_index + 1 == data.instructions.len() {
+            return;
+        }
+        let _ = data.match_current_instruction(true);
+        data.instruction_index += 1;
+        let index = data.instruction_index.clone();
+        let output = data.get_output().clone();
+        let mut memory_string = "|".to_string();
+        let mut instructions_string= data.instructions.clone();
+        if instructions_string.get(index.. index+1).unwrap() != "\n" {
+            instructions_string.replace_range(index.. index+1, "▋");
+        }
+        for cell in &data.machine.tape {
+            memory_string.push_str(&format!("{}|", cell));
+        }
+        cursive.call_on_name("standard_output", |standard_output: &mut TextView| {
+            standard_output.set_content(output);
+        });
+        cursive.call_on_name("memory", |memory: &mut TextView| {
+            memory.set_content(memory_string);
+        });
+        cursive.call_on_name("instructions", |instructions: &mut TextView| {
+            instructions.set_content(instructions_string);
+        });
+    }
+}
 pub fn visualize (parser: Parser) {
     let mut siv = Cursive::default();
     siv.add_global_callback('q',|s| s.quit());
@@ -14,37 +43,7 @@ pub fn visualize (parser: Parser) {
                 cursive.quit();
             }
         ))
-        .child(Button::new(
-            "Step One",
-            |cursive| {
-                if let Some(data) = cursive.user_data::<Parser>() {
-                    if data.instruction_index + 1 == data.instructions.len() {
-                        return;
-                    }
-                    let _ = data.match_current_instruction(true);
-                    data.instruction_index += 1;
-                    let index = data.instruction_index.clone();
-                    let output = data.get_output().clone();
-                    let mut memory_string = "|".to_string();
-                    let mut instructions_string= data.instructions.clone();
-                    if instructions_string.get(index.. index+1).unwrap() != "\n" {
-                        instructions_string.replace_range(index.. index+1, "▋");
-                    }
-                    for cell in &data.machine.tape {
-                        memory_string.push_str(&format!("{}|", cell));
-                    }
-                    cursive.call_on_name("standard_output", |standard_output: &mut TextView| {
-                        standard_output.set_content(output);
-                    });
-                    cursive.call_on_name("memory", |memory: &mut TextView| {
-                        memory.set_content(memory_string);
-                    });
-                    cursive.call_on_name("instructions", |instructions: &mut TextView| {
-                        instructions.set_content(instructions_string);
-                    });
-                }
-            }
-        ));
+        .child(Button::new("Step One", step));
 
     let main = LinearLayout::vertical()
         .child(TextView::new("Your code:").align(Align::center()))
