4258d8abcd5dda3ae77649cea081e3e54c6b36e3
Jaden Arceneaux <arceneauxjaden@gmail.com>
2024-02-10 13:46:29 -0800

    docs

--- src/visualize/mod.rs
+++ src/visualize/mod.rs
@@ -4,6 +4,7 @@ use cursive::view::{Nameable, Resizable};
 use cursive::views::{Button, Dialog, LinearLayout, ScrollView, TextView};
 use crate::parser::Parser;
 
+/// Iterate program to next instruction and update TUI
 fn step(cursive: &mut Cursive) {
     if let Some(data) = cursive.user_data::<Parser>() {
         if data.get_instruction_index() + 1 == data.get_instructions().len() {
@@ -31,6 +32,8 @@ fn step(cursive: &mut Cursive) {
         });
     }
 }
+
+/// Visualize execution of program
 pub fn visualize (parser: Parser) {
     let mut siv = Cursive::default();
     siv.add_global_callback('q',|s| s.quit());
